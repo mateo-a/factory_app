8cdbca04e88458c61f1299aad950a36c3bce9d53
mateo-a <alexander.urrego@gmail.com>
2021-06-14 19:37:02 -0500

    [ADD] Temporary detail_factory page

---
+++ lib/src/pages/detail_factory.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DetailFactory extends StatefulWidget {
+  @override
+  _DetailFactoryState createState() => _DetailFactoryState();
+}
+
+class _DetailFactoryState extends State<DetailFactory> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
