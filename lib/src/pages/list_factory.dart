0ae8de41b53de78b00f5298f0ccb3f397f90d6ea
mateo-a <alexander.urrego@gmail.com>
2021-06-14 23:21:23 -0500

    [Update] Showing firebase data in list_factory

--- lib/src/pages/list_factory.dart
+++ lib/src/pages/list_factory.dart
@@ -1,26 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:formvalidation/src/models/factory_model.dart';
+import 'package:formvalidation/src/providers/factories_provider.dart';
 
 class ListFactory extends StatelessWidget {
+  final factoriesProvider = new FactoriesProvider();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Fabrica'),
+        title: Text('Fabricas'),
+        actions: [
+          IconButton(
+            onPressed: () => Navigator.pushNamed(context, 'agregar'),
+            icon: Icon(Icons.add_business),
+          )
+        ],
       ),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: EdgeInsets.all(15.0),
-          child: Form(
-            child: Column(
-              children: [
-                // _crearNombre(),
-                // _crearDireccion(),
-                // _crearBoton(),
-              ],
-            ),
-          ),
-        ),
-      ),
+      body: _crearListado(),
     );
+  }
+
+  Widget _crearListado() {
+    return FutureBuilder(
+      future: factoriesProvider.cargarFabricas(),
+      builder:
+          (BuildContext context, AsyncSnapshot<List<FactoryModel>> snapshot) {
+        if (snapshot.hasData) {
+          final fabricas = snapshot.data;
+
+          return ListView.builder(
+            itemCount: fabricas.length,
+            itemBuilder: (context, i) => _crearItem(context, fabricas[i]),
+          );
+        } else {
+          return Center(child: CircularProgressIndicator());
+        }
+      },
+    );
+  }
+
+  Widget _crearItem(BuildContext context, FactoryModel fabricas) {
+    return Dismissible(
+      key: UniqueKey(),
+      background: Container(color: Colors.red),
+      onDismissed: (direction) {
+        factoriesProvider.borrarFabrica(fabricas.id);
+      },
+      child: ListTile(
+          leading: Icon(Icons.business, size: 40.0),
+          title: Text('${fabricas.nombre}'),
+          subtitle: Text('Teléfono: ${fabricas.telefono}'),
+          isThreeLine: true,
+          onTap: () => Navigator.pushNamed(context, 'detalle')),
+    );
+  }
+
+  _crearBoton(BuildContext context) {
+    return FloatingActionButton(
+        child: Icon(Icons.add),
+        backgroundColor: Colors.green,
+        onPressed: () => Navigator.pushNamed(context, 'agregar'));
   }
 }
